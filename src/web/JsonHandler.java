8d020356d196a81f216558501c9e8dca99aec65e
Patrick NIYONGABO <patrickniyongabo@gmail.com>
2015-04-03 11:53:11 -0500

    ABle to read string from urls

--- src/web/JsonHandler.java
+++ src/web/JsonHandler.java
@@ -2,32 +2,37 @@ package web;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
+
 public class JsonHandler {
 	UrlOpener opener = new UrlOpener();
+	String scheme = "http://", authority  = "api.openweathermap.org";
+	String pathForecast = "/data/2.5/forecast/daily?q=", pathCurrent = "/data/2.5/find?q=";	
+	String querryImperial = ",%20us&units=imperial&cnt=", querryMetric = ",%20us&units=metric&cnt=";
+	String querryLike = "&type=like", querryAccurate = "&type=accurate";
+	String json;
 	
 	public JsonHandler(){}
 	
 	public String generateString(String zipCode, String numberOfDays) throws IOException{
-		String linkToHandle = "link from openweathermap";
-		//typical link looks like the one below
-		//http://api.openweathermap.org/data/2.5/forecast/daily?q=72032,%20us&units=metric&cnt=16
+		String linkToHandle = scheme+authority+pathForecast+zipCode+querryImperial+numberOfDays+querryAccurate;
+		//typical link looks like these two below
+		//http://api.openweathermap.org/data/2.5/forecast/daily?q=72032,%20us&units=imperial&cnt=16&type=accurate
 		BufferedReader data = opener.openUrl(linkToHandle);
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		String line;
+		while((line = data.readLine()) != null){
+		    json = line;
+		}
+		//ObjectMapper mapper = new ObjectMapper();
+		//mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		//mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		String json = mapper.writeValueAsString(data);
+		//String json = mapper.writeValueAsString(data);
 		System.out.println(json);
 		return json;
 	}
-	
-
-
-	
-	
-
 }
