c66c4958d537f8114d63ef1b9e4955ceeafb0514
Patrick NIYONGABO <patrickniyongabo@gmail.com>
2015-04-06 17:33:58 -0500

    zipcodeData

--- src/web/ZipcodeData.java
+++ src/web/ZipcodeData.java
@@ -6,11 +6,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class ZipcodeData {
-
-	public DataAsStringGenerator datagenerator;
 	
-	public ArrayList<Day> getDays (String zipCode, int days) throws IOException{
-		datagenerator = new DataAsStringGenerator();
+	public static ArrayList<Day> getDays (String zipCode, int days) throws IOException{
+		DataAsStringGenerator datagenerator = new DataAsStringGenerator();
 		String data = datagenerator.generateStringForCurrent(zipCode, Integer.toString(days));
 		return JsonParser.parseJson(data);
 	}
